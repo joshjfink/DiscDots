05b577faebd5193972a5a353861c83474e0c2efc
joshua-james-f <Joshua@wl-10-190-213-33.wireless.duke.edu>
2015-12-09 04:10:09 -0500

    Corrected structure of main r script

--- R/DiscPlot.R
+++ R/DiscPlot.R
@@ -1,30 +1,19 @@
 ##########
-library(ggplot2)
-library(gtable)
-
-ones <- sample.int(10, size=10,replace = TRUE)
-n <- length(ones)
-m <- matrix(nrow=n, ncol=max(ones))
-
-# Create gauge matrix 
-for(i in 1:n){
-	g_len <- max(ones)
-	t_ <- rep(1, ones[i])
-	f_ <- rep(0, g_len-ones[i])
-	ifelse(ones[i]==g_len, 
-		m[i,] <- rep(2, ones[i]),
-		m[i,] <- factor(t(c(t_, f_)))
-		)
+#' Create Gauge Matrix
+#'
+#' @param values Number of  filled in dots for each variable
+#' @return matrix of 2's and 1's to be fed into plotting functions.
+gen_mat <- function(values, ...) {
+  n <- length(values)
+  m <- matrix(nrow=n, ncol=max(values))
+  for(i in 1:n){
+    g_len <- max(values)
+    t_ <- rep(1, values[i])
+    f_ <- rep(0, g_len-values[i])
+    ifelse(values[i]==g_len,
+           m[i,] <- rep(2, values[i]),
+           m[i,] <- factor(t(c(t_, f_)))
+    )
+    return(m)
+  }
 }
-
-mdat <- data.frame(t(m))
-mdat$var <- factor(1:ncol(mdat))
-mdat2 <- melt(mdat)
-head(mdat2)
-
-# Set Colors
-sgcols <- c("#E4E7EC", "#3A7AA6")
-
-# Run Test
-ggplot(data = mdat2, aes(x = variable, y = as.numeric(var)))+ geom_point(aes(color=factor(value)), size=10) +theme( axis.text.x=element_blank(),panel.grid.major.y = element_blank(), axis.line = element_blank(), panel.grid.major.x = element_blank(), panel.grid.minor.y = element_blank()) + scale_color_manual(values=sgcols) + coord_flip()+theme(axis.line.y=element_blank(), axis.ticks.y=element_blank()) + theme_gdocs2()+theme( axis.text.x=element_blank() ,panel.grid.major.y = element_blank()) +theme(legend.position="none", axis.line = element_blank())
-
